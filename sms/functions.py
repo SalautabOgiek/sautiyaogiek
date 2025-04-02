e12857144436c02d8041457c3f5132089be08a82
Cody Thornell <grcodeman@gmail.com>
2025-04-02 15:23:37 -0400

    sms function setup

---
+++ sms/functions.py
@@ -0,0 +1,7 @@
+import requests
+
+def send_bulk_sms(api_key, username, message, senderId, phoneNumbers, maskedNumber=None, telco=None):
+    return ""
+
+def fetch_sms(api_key, username, lastReceivedId=None):
+    return ""
