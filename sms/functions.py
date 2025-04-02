0d0c39a10e7da93432626244203bc34a988081be
Cody Thornell <grcodeman@gmail.com>
2025-04-02 15:35:50 -0400

    added send bulk sms function

--- sms/functions.py
+++ sms/functions.py
@@ -1,7 +1,29 @@
 import requests
 
+# Send Bulk SMS
+# https://developers.africastalking.com/docs/sms/sending/bulk
 def send_bulk_sms(api_key, username, message, senderId, phoneNumbers, maskedNumber=None, telco=None):
-    return ""
+
+    # prep request (url, header, payload)
+    url = "https://api.africastalking.com/version1/messaging/bulk"
+
+    header = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "apiKey": api_key
+    }
+
+    payload = {
+        "username": username,
+        "message": message,
+        "senderId": senderId,
+        "phoneNumbers": phoneNumbers,
+    }
+
+    # call request and return json response
+    response = requests.post(url, json=payload, headers=header)
+    return response.json()
 
 def fetch_sms(api_key, username, lastReceivedId=None):
     return ""
+
