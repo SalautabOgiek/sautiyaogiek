2c33ab688fe1f2a4216907ba372aa2ec50f8c41d
Khang Nguyen <dsy1090@wmich.edu>
2025-04-09 17:18:07 -0400

    test on cli

---
+++ ollama_test.py
@@ -0,0 +1,16 @@
+from ollama_func import send_ollama
+
+def main():
+    print("Welcome to the chatbot! (type 'exit' to quit)\n")
+    model = 'mistral'
+
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'exit':
+            break
+
+        response = send_ollama(model, user_input)
+        print("Ollama:", response, "\n")
+
+if __name__ == '__main__':
+    main()
