5f605114a496b8550bc77ef6e9e5c053bdd26884
Khang Nguyen <dsy1090@wmich.edu>
2025-05-01 21:52:28 -0400

    embedding func

--- embedding_func.py
+++ embedding_func.py
@@ -1,4 +1,4 @@
-from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_ollama import OllamaEmbeddings
 
 def get_embedding_function(input_model):
     embeddings = OllamaEmbeddings(model=input_model)
