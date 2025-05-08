69513a7764c6ef6d0fc0abe88d95f8ab7d18355d
Khang Nguyen <dsy1090@wmich.edu>
2025-05-08 06:31:37 -0500

    PROMPT UPDATE

--- query_data.py
+++ query_data.py
@@ -10,13 +10,19 @@ DB_PATH = "database"
 
 # prompt feeding the model to get desired response, context is fetch from database, and question is user input
 PROMPT_TEMPLATE = """
-Answer the question in less than 190 characters based only on the following context:
+You are an informational agent dedicated to educating users about the Ogiek community. Given the most relevant documentations and a question either in English or Swahili from the user, 
+you must answer the question in English if asked in English, in Swahili if asked in Swahili. Your answer should be short, no more than 190 characters. If a question is asked that does not relate to the documentation given,
+tell the users you can only answer on what you are trained for.
 
+Here are the documentations:
+ 
 {context}
-
+ 
 ---
+ 
+Here  is the question asked by the user: {question}
 
-Answer the question in less than 190 characters based on the above context, DO NOT GIVE ANSWERS THAT IS NOT IN THE CONTEXT: {question}
+AGAIN, THE MOST IMPORTANT THINGS ARE: ANSWER IN LESS THAN 190 CHARACTERS, IN SWAHILI IF THE QUESTION IS IN SWAHILI, and only give answer derived from the source.
 """
 
 
@@ -39,7 +45,7 @@ def query_rag(query_text: str):
     db = Chroma(persist_directory=DB_PATH, embedding_function=embedding_function)
 
     # search the DB.
-    results = db.similarity_search_with_score(query_text, k=5) 
+    results = db.similarity_search_with_score(query_text, k=3) 
 
 
     # Make sure response is relevant
@@ -50,7 +56,7 @@ def query_rag(query_text: str):
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
+    # print(prompt)
 
 
     ollama_model = OllamaLLM(model = model)
